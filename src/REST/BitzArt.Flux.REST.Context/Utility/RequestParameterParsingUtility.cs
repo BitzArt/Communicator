e128ac313c1b5956d2766963a82cac73171ae9ea
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2025-01-20 10:51:33 +0400

    Reset RequestParameterParsingUtility

--- src/REST/BitzArt.Flux.REST.Context/Utility/RequestParameterParsingUtility.cs
+++ src/REST/BitzArt.Flux.REST.Context/Utility/RequestParameterParsingUtility.cs
@@ -6,34 +6,32 @@ namespace BitzArt.Flux;
 
 internal partial class RequestParameterParsingUtility
 {
-    public static RequestUrlParameterParsingResult ParseRequestUrl(string path, IRestRequestParameters parameters)
+    public static RequestUrlParameterParsingResult ParseRequestUrl(string path, object[]? parameters)
     {
+        var logBuilder = new StringBuilder();
+
         var matches = ParameterRegex().Matches(path);
         if (matches.Count == 0) return new RequestUrlParameterParsingResult(path, string.Empty);
 
-        var requiredCount = matches.Count;
-        var foundCount = parameters.Parameters.Count;
+        if (parameters is null) throw new ParametersNotFoundException();
 
-        if (requiredCount != foundCount) 
-            throw new ParameterCountDidNotMatchException(foundCount, requiredCount);
+        var requiredCount = matches.Count;
+        var foundCount = parameters.Length;
+
+        if (requiredCount != foundCount) throw new ParameterCountDidNotMatchException(foundCount, requiredCount);
 
         var result = path;
-
-        var logBuilder = new StringBuilder();
         logBuilder.Append('\n');
 
-        foreach (Match match in matches)
+        for (int counter = 0; counter < requiredCount; counter++)
         {
-            var parameterName = match.Groups[1].Value;
-            var parameter = parameters.Parameters.FirstOrDefault(p => p.Key == parameterName);
+            var match = matches[counter];
+            var paramName = match.Groups[1].Value;
+            var value = parameters[counter].ToString();
+            result = result.Replace(match.Value, value);
 
-            // TODO: Review this condition
-            if (parameter.Equals(default(KeyValuePair<string, object>)))
-                throw new ParameterNotFoundException(parameterName);
-
-            result = result.Replace(match.Value, parameter.Value!.ToString());
-
-            logBuilder.Append($"{parameterName}: {parameter.Value}\n");
+            if (counter > 0) logBuilder.Append("; ");
+            logBuilder.Append($"{paramName}: {value}");
         }
 
         return new RequestUrlParameterParsingResult(result, logBuilder.ToString());
@@ -43,12 +41,14 @@ internal partial class RequestParameterParsingUtility
     private static partial Regex ParameterRegex();
 }
 
-file class ParameterCountDidNotMatchException(int found, int required)
-    : Exception($"Number of parameters in a request ({found}) did not match number of required parameters ({required}) for this endpoint.")
+file class ParametersNotFoundException : Exception
 {
+    public ParametersNotFoundException()
+        : base("Parameters are specified in endpoint configuration but not found in the request.")
+    { }
 }
 
-file class ParameterNotFoundException(string parameterName) 
-    : Exception($"Parameter '{parameterName}' is specified in endpoint configuration but not found in the request.")
+file class ParameterCountDidNotMatchException(int found, int required)
+    : Exception($"Number of parameters in a request ({found}) did not match number of required parameters ({required}) for this endpoint.")
 {
 }
